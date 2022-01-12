56aba727c93fe15dc6c680f4d8609aab0ef23939
mayankcodeops <mayank@codeops.tech>
2022-01-12 13:28:27 +0530

    Fized Directory Path for JSON artifacts

--- app/api/artifacts.py
+++ app/api/artifacts.py
@@ -2,15 +2,19 @@ from flask import jsonify, request, url_for, abort
 from app.api import bp
 from app.api.errors import bad_request
 
+import os
+
+artifact_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'artifacts/')
+
 
 @bp.route('/artifact/<object_id>', methods=['GET'])
 def get_artifact(object_id):
     try:
-        with open('artifacts/' + object_id+'.json', 'r') as f:
+        with open(artifact_dir + object_id + '.json', 'r') as f:
             artifact = f.read()
     except OSError as err:
-        return bad_request(f'No artifact found with objectID: {object_id}')
+        return bad_request(f'No artifact found with objectID: {object_id}, {err}')
     else:
-        return jsonify(artifact)
+        return artifact
 
 
