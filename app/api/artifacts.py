234419158258b63cad9251838b42a185cdd490f0
mayankcodeops <mayank@codeops.tech>
2022-01-12 12:59:29 +0530

    Added API routes

---
+++ app/api/artifacts.py
@@ -0,0 +1,16 @@
+from flask import jsonify, request, url_for, abort
+from app.api import bp
+from app.api.errors import bad_request
+
+
+@bp.route('/artifact/<object_id>', methods=['GET'])
+def get_artifact(object_id):
+    try:
+        with open(object_id+'.json', 'r') as f:
+            artifact = f.read()
+    except OSError as err:
+        return bad_request(f'No artifact found with objectID: {object_id}')
+    else:
+        return jsonify(artifact)
+
+
