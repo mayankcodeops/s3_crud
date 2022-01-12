666a96ee188b7aab9a8c5e081adab0daab54cb5f
mayankcodeops <mayank@codeops.tech>
2022-01-12 14:10:59 +0530

    Added API route for artifact creation

--- app/api/artifacts.py
+++ app/api/artifacts.py
@@ -3,6 +3,7 @@ from app.api import bp
 from app.api.errors import bad_request
 
 import os
+import json
 
 artifact_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'artifacts/')
 
@@ -17,4 +18,20 @@ def get_artifact(object_id):
     else:
         return jsonify({object_id: artifact})
 
+
+@bp.route('artifact/<object_id>', methods=['POST'])
+def create_artifact(object_id):
+    artifact = request.get_json() or {}
+    if os.path.isfile(os.path.join(artifact_dir, object_id+ '.json')):
+        return bad_request(f'Artifact with objectID: {object_id} already exists. Cannot create resource.')
+    else:
+        try:
+            with open(artifact_dir + object_id + '.json', 'wb') as file:
+                file.write(json.dumps(artifact).encode())
+        except OSError as err:
+            return bad_request(f'Something went wrong, please try again')
+        else:
+            response = jsonify({object_id: artifact})
+            response.status_code = 201
+            return response
 
