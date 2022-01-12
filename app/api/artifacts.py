6a5722da3be0b3226945ca2b1ad6c66361f58159
mayankcodeops <mayank@codeops.tech>
2022-01-12 13:14:59 +0530

    Fixed typo

--- app/api/artifacts.py
+++ app/api/artifacts.py
@@ -6,7 +6,7 @@ from app.api.errors import bad_request
 @bp.route('/artifact/<object_id>', methods=['GET'])
 def get_artifact(object_id):
     try:
-        with open(object_id+'.json', 'r') as f:
+        with open('artifacts/' + object_id+'.json', 'r') as f:
             artifact = f.read()
     except OSError as err:
         return bad_request(f'No artifact found with objectID: {object_id}')
